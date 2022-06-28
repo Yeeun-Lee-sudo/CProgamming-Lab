33d60415cb3b77abed6db2600e467a5bc2b489ab
Yeeun-Lee-sudo <yesilver@ewhain.net>
2022-06-28 14:34:57 +0900

    수업시간 내용 백업

---
+++ 0628/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a = 2;
+    int b = 5;
+
+    printf("%d + %d = %d\n", a, b, a+b);
+    printf("%d - %d = %d\n", a, b, a-b);
+    printf("%d * %d = %d\n", a, b, a*b);
+    printf("%d / %d = %d\n", a, b, a/b);
+
+    return 0;
+}
