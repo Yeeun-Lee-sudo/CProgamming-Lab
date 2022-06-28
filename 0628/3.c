34abb5da432bee5ef7436f21a4c852b29bacf0d5
Yeeun-Lee-sudo <yesilver@ewhain.net>
2022-06-28 14:42:47 +0900

    3

---
+++ 0628/3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b;
+    int sum;
+
+    /*
+    scanf("%d %d", &a, &b);
+
+    printf("%d + %d = %d\n", a, b, a+b);
+    printf("%d - %d = %d\n", a, b, a-b);
+    printf("%d * %d = %d\n", a, b, a*b);
+    printf("%d / %d = %d\n", a, b, a/b);
+    */
+    
+    return 0;
+}
